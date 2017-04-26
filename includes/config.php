1a33e17dc6932613eab9a7657c87ffab511517a2
polatmert <husnumertpolatt@gmail.com>
2017-04-26 03:08:49 +0300

    Sql Injection
    sql injection update

--- includes/config.php
+++ includes/config.php
@@ -1,14 +1,27 @@
-<?php 
-$baglanti = @mysql_connect('localhost', 'root', '');
-$veritabani = @mysql_select_db('yazlabproje');
- 
-if($baglanti && $veritabani) {  
-//echo "baglanti kuruldu";
-} 
-else {
-   echo 'Bağlantı kurulamadı.';
-}
-mysql_query("SET NAMES utf8");
+<?php
+$db_adres="localhost";
+$db_user="root";
+$db_pass="";
+$db_tablo="yazlabproje";
 
-session_start();
+$conn = mysqli_connect($db_adres,$db_user,$db_pass,$db_tablo);
+
+
+if(!$conn)
+{
+	die("Bağlantı Hatasi".mysqli_error());
+	}
+
+$db_select=mysqli_select_db($conn,$db_tablo);
+
+if(!$db_select)
+{
+	die("Bağlantı Hatasi select".mysqli_error());
+	}
+mysqli_query($conn,"SET NAMES UTF8");
+
+error_reporting(E_ALL ^ E_DEPRECATED);
+
+
+
 ?>
--- index.php
+++ index.php
@@ -1,8 +1,41 @@
+	<form  action="#" name="kayit" method="GET">
+       id:<input type="text" name="id"><br /><br />
+           
+      <input name="gonder" type="submit">
+    </form>
 
+<?php 
+ 
+include("includes/config.php");?>
 
-	<form  action="login.php" name="kayit" method="GET">
-       Username:<input type="text" name="username"><br /><br />
-      Password:&nbsp;<input type="password" name="password"><br/><br />
-      
-      <input type="submit">
-   </form>
+<?php 
+
+	if( isset( $_GET[ 'gonder' ] ) ) { 
+	
+    // Get input 
+    $id = $_GET[ 'id' ]; 
+	echo"mert __ $id __";
+    // Check database 
+		$query  = "SELECT * FROM users WHERE id =$id;"; 
+	//$query  = "SELECT * FROM users WHERE id = 2 union select 0,1,version(),3,4;"; 
+	echo"<br>query-- $query";
+    $result = mysqli_query($conn,  $query ) or die( '<pre>' . ((is_object($conn)) ? mysqli_error($conn) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '</pre>' );
+
+    // Get results 
+    while( $row = mysqli_fetch_assoc( $result ) ) { 
+        // Get values 
+			$idsi=$row["id"];
+        $username = $row["username"]; 
+        $password  = $row["password"];
+		$tel=$row["telephone"];
+		$mail=$row["email"];
+
+        // Feedback for end user 
+        echo "<pre>ID: {$idsi}<br />username: {$username}<br />password: {$password}<br>{$tel}<br>{$mail}</pre>"; 
+    } 
+
+    mysqli_close($conn); 
+	echo"mert";
+} 	
+
+?>
