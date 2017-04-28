abc70a32a80a9bff5b08ae46b09d9aba11caab99
polatmert <husnumertpolatt@gmail.com>
2017-04-28 17:01:31 +0300

    SQL Injection
    Added solution for weaknesses

--- admin.php
+++
@@ -1,8 +0,0 @@
-<?php 
- 
-include("includes/config.php");
-
-    echo "<center>Admin sayfasina hosgeldiniz..! ";
-    echo "<a href=logout.php>Guvenli cikis</a></center>";
-
-?>
---
+++ includes/header.php
@@ -0,0 +1,18 @@
+<head>
+    <title>Vulnerable</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link href="css/style.css" rel="stylesheet" />
+    <link href="css/bootstrap.css" rel="stylesheet" />
+    <link  rel="stylesheet" href="font-awesome/css/font-awesome.css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+   <style>
+    pre {
+    display: block;
+    font-family: monospace;
+    white-space: pre;
+    line-height: 0.8;
+    }
+   </style>
+</head>
